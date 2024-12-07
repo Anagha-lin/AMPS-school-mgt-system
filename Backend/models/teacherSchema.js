025dce6e81932d584967fdb56e24a39ab5d10f0a
Anagha-lin <135278326+Anagha-lin@users.noreply.github.com>
2024-12-07 21:08:27 +0000

    backened

--- Backend/models/teacherSchema.js
+++ Backend/models/teacherSchema.js
@@ -1,49 +1,21 @@
-const mongoose = require("mongoose")
+import mongoose from "mongoose";
 
 const teacherSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        default: "Teacher"
-    },
-    school: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'admin',
-        required: true,
-    },
-    teachSubject: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'subject',
-    },
-    teachSclass: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'sclass',
-        required: true,
-    },
-    attendance: [{
-        date: {
-            type: Date,
-            required: true
-        },
-        presentCount: {
-            type: String,
-        },
-        absentCount: {
-            type: String,
-        }
-    }]
-}, { timestamps: true });
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+});
 
-module.exports = mongoose.model("teacher", teacherSchema)
+
+export const Teacher = mongoose.model('Teacher', teacherSchema);
+
